060203d5cd355b4a6f94bf50010df2a59bc219c9
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-02-10 14:23:24 +0100

    Upstream literature code

--- scenarios/diabetes_analysis.py
+++ scenarios/diabetes_analysis.py
@@ -1,14 +1,14 @@
-"""Demonstrate running search, retrival, annotation, and analysis with recent diabetes literature."""
+"""Run search, retrival, annotation, and analysis with recent diabetes literature."""
 
 import click
+from bioliterature.analyze import count_cooccurrences, count_references
+from bioliterature.annotate import annotate_abstracts_from_search
 from tabulate import tabulate
 
 from biolexica import load_grounder
-from biolexica.literature import annotate_abstracts_from_search
-from biolexica.literature.analyze import count_cooccurrences, count_references
 
 
-def _main():
+def _main() -> None:
     query = "diabetes"
     grounder = load_grounder("phenotype")
     annotated_articles = annotate_abstracts_from_search(query, grounder=grounder, limit=300)
@@ -18,7 +18,7 @@ def _main():
     click.echo("\nOccurrences")
     click.echo(
         tabulate(
-            [(r.curie, name, count) for (r, name), count in reference_counter.most_common(10)],
+            [(r.curie, r.name, count) for r, count in reference_counter.most_common(10)],
             headers=["Reference", "Name", "Count"],
             tablefmt="github",
         )
@@ -28,10 +28,10 @@ def _main():
     click.echo(
         tabulate(
             [
-                (l_r.curie, l_n, r_r.curie, r_n, count)
+                (left.curie, left.name, right.curie, right.name, count)
                 for (
-                    (l_r, l_n),
-                    (r_r, r_n),
+                    left,
+                    right,
                 ), count in co_occurrence_counter.most_common(10)
             ],
             headers=[
--- tests/test_lexica.py
+++ tests/test_lexica.py
@@ -5,7 +5,6 @@ from pathlib import Path
 
 import biolexica
 from biolexica.api import PREDEFINED
-from biolexica.literature import annotate_abstracts_from_pubmeds, annotate_abstracts_from_search
 
 HERE = Path(__file__).parent.resolve()
 ROOT = HERE.parent
@@ -31,43 +30,8 @@ class TestLexica(unittest.TestCase):
 
     def test_ground_cells(self):
         """Test grounding cells."""
-        res = self.cell_grounder.ground("hela")
+        res = self.cell_grounder.get_matches("hela")
         self.assertIsInstance(res, list)
         self.assertEqual(1, len(res))
-        self.assertEqual("cellosaurus", res[0].term.db)
-        self.assertEqual("0030", res[0].term.id)
-
-    def test_exclude_doid_4(self):
-        """Test that exclusion during construction of the phenotype index works properly."""
-        pubmed_id = "38279949"
-        results = annotate_abstracts_from_pubmeds(
-            [pubmed_id], grounder=self.phenotype_grounder, show_progress=False
-        )
-        articles_with_doid_4 = {
-            result.pubmed
-            for result in results
-            for ref, _name in result.count_references()
-            if ref.curie == "doid:4"
-        }
-        self.assertEqual(
-            set(),
-            articles_with_doid_4,
-            msg="No articles should contain the reference `doid:4` for the top-level disease annotation, "
-            "since this should be filtered out during construction of the lexical index.",
-        )
-
-    def test_search_alz(self):
-        """Test searching and annotating Alzheimer's docs gets a desired annotation."""
-        results = annotate_abstracts_from_search(
-            "alzheimers",
-            grounder=self.phenotype_grounder,
-            limit=20,
-            show_progress=False,
-        )
-        self.assertTrue(
-            any(
-                ref.curie == "doid:10652"  # this is the DOID term for Alzheimer's disease
-                for result in results
-                for ref, _name in result.count_references()
-            )
-        )
+        self.assertEqual("cellosaurus", res[0].prefix)
+        self.assertEqual("0030", res[0].identifier)
